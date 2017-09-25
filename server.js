de9d34abb8bde3ee87f97a0cd1ff2b0c8fc114f7
leondo <leondo123@gmail.com>
2017-09-25 10:17:17 -0600

    needs to be tested

--- server.js
+++ server.js
@@ -37,14 +37,14 @@ app.post('/', function (req, res){
     	seller(message)
     	return 'your money'
     }
-    else if (message.represent === 'buyer' && message.command === 'I_agree_to_the_contract'){
-    	console.log('\n The message is from the buyer and he/she agrees to the contract...')
+    else if (message.represent === 'buyer' && message.command === 'I_agree_to_the_contract' && contract[message.uuid] !== undefined){
+    	console.log('\n The message is from the buyer & he/she agrees to the contract & the contract exists...')
     	console.log('\n going to contract_agreement() logic...')
     	contract_agreement(message)
     	return 'your money'
     }
-    else if (message.represent === 'buyer' && message.command === 'complete_transaction'){
-    	console.log('\n The message from the buyer and says complete the transaction')
+    else if (message.represent === 'buyer' && message.command === 'complete_transaction' && contract[message.uuid] !== undefined){
+    	console.log('\n The message from the buyer & he/she says `complete the transaction` & the contract is agreed upon...')
     	console.log('\n going to complete_transaction() logic...')
     	complete_transaction(message)
     	return 'your money'
@@ -61,19 +61,21 @@ app.post('/', function (req, res){
 function contract_agreement(message){
 	console.log('\n Starting the contract agreement logic:')
 
-	if (message.buyer_amount >= 2 * contract[message.uuid].seller_amount){
-		console.log("\n The buyer amount is greater than the seller amount + seller collateral (2 * seller amount)...")		
-		console.log('\n ...adding buyer information to the contract')
+	if (message.buyer_amount >= 2 * contract[message.uuid].seller_amount && contract[message.uuid].buyer_amount === undefined){
+		console.log('\n The buyer amount is greater than the seller amount + seller collateral (2 * seller amount) & the item has not been bought...')
+        console.log('\n ...adding buyer information to the contract')
 		contract[message.uuid].buyer_address = message.buyer_address
 		contract[message.uuid].buyer_amount = message.buyer_amount
 
-    	console.log('\n Your contract is: ', contract[message.uuid])
-		console.log('\n Contract is now locked in. Message the seller to ship the item. When the item arrives, complete contract')
+    	console.log('\n The contract = ', contract[message.uuid])
+		console.log(`\n The contract is now locked in. Message the seller to ship the item. When the item arrives, complete contract`)
 		return 'and wait for the item...'
 	
 	} else {
 	
-		console.log("\n The buyer amount is too low. Return funds.")
+		console.log("\n The buyer amount is too low or the item has already been bought.")
+        console.log(contract[message.uuid])
+        console.log('Return funds.')
 		return 'your funds'
 	
 	}
@@ -83,16 +85,28 @@ function contract_agreement(message){
 
 function complete_transaction(message){
 	console.log('\n Starting complete transaction logic:')
-
-	console.log('\n pay the seller the value 2 times the seller amount (1x for the value of the item and 1x for the collateral)')
-	var seller_pay = 2 * contract[message.uuid].seller_amount
-
-	console.log('\n pay the buyer the rest of his collateral')
-	var buyer_pay = contract[message.uuid].buyer_amount - contract[message.uuid].seller_amount
+    // message.buyer_amount >= 2 * contract[message.uuid].seller_amount
     
-    console.log('\n Your contract is: ', contract[message.uuid])
-	console.log('\n seller gets paid ', seller_pay)
-	console.log('\n buyer gets paid ', buyer_pay)
+    if (contract[message.uuid].buyer_amount >= 2 * contract[message.uuid].seller_amount){
+        console.log('\n Verifying the contract_agreement...we gud.')
+
+    	console.log('\n Pay the seller the value 2 times the seller amount (1x for the value of the item and 1x for the collateral)')
+    	var seller_pay = 2 * contract[message.uuid].seller_amount
+
+    	console.log('\n pay the buyer the rest of his collateral')
+    	var buyer_pay = contract[message.uuid].buyer_amount - contract[message.uuid].seller_amount
+        
+        console.log('\n Your contract is: ', contract[message.uuid])
+    	console.log('\n seller gets paid ', seller_pay)
+    	console.log('\n buyer gets paid ', buyer_pay)
+    } else {
+
+        console.log("\n The contact hasn't been agreed upon.")
+        console.log(contract[message.uuid])
+        console.log('Return funds.')
+        return 'your funds'
+
+    }
 
 }
 
@@ -113,7 +127,9 @@ function seller(message){
     	console.log('\n The item is now listed on the contract. Waiting on buyer...')
 	} 
 	else {
-		console.log('\n Seller information is already listed in the contract. Returning your money.')
+		console.log('\n Seller information is already listed in the contract')
+        console.log(contract[message.uuid])
+        console.log('Returning your money.')
 		return 'your money'
 	}
 }
