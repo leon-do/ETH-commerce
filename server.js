bae38281d73c7b4650d8f769a4e98add65ef82ea
leondo <leondo123@gmail.com>
2017-09-25 11:10:32 -0600

    tested

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ var transation = {
         },
     'uuid2':
         {
-            'seller_amount'    : 2,
-            'seller_address'  : '0x98usdji'
-            'buyer_amount'    : 4,
-            'buyer_address'  : '0xf8h2ja2m'
+            seller_amount: '5',
+            seller_address: '0xsSsSs',
+            buyer_address: '0xbBbBb',
+            buyer_amount: '10'
         },        
 };
 */
@@ -35,19 +35,16 @@ app.post('/', function (req, res){
     	console.log('\n The message is from the seller...')
     	console.log('\n going to seller logic...')
     	seller(message)
-    	return 'your money'
     }
     else if (message.represent === 'buyer' && message.command === 'I_agree_to_the_contract' && contract[message.uuid] !== undefined){
     	console.log('\n The message is from the buyer & he/she agrees to the contract & the contract exists...')
     	console.log('\n going to contract_agreement() logic...')
     	contract_agreement(message)
-    	return 'your money'
     }
     else if (message.represent === 'buyer' && message.command === 'complete_transaction' && contract[message.uuid] !== undefined){
     	console.log('\n The message from the buyer & he/she says `complete the transaction` & the contract is agreed upon...')
     	console.log('\n going to complete_transaction() logic...')
     	complete_transaction(message)
-    	return 'your money'
     }
     else {
     	console.log('\n message is invalid. Returning your money.')
@@ -68,15 +65,15 @@ function contract_agreement(message){
 		contract[message.uuid].buyer_amount = message.buyer_amount
 
     	console.log('\n The contract = ', contract[message.uuid])
-		console.log(`\n The contract is now locked in. Message the seller to ship the item. When the item arrives, complete contract`)
+		console.log(`\n The contract is now locked in. Message the seller to ship the item. When the item arrives, click 'complete transacton'`)
 		return 'and wait for the item...'
 	
 	} else {
 	
 		console.log("\n The buyer amount is too low or the item has already been bought.")
         console.log(contract[message.uuid])
-        console.log('Return funds.')
-		return 'your funds'
+        console.log('Return buyer money.')
+		return 'buyer money'
 	
 	}
 
@@ -85,7 +82,6 @@ function contract_agreement(message){
 
 function complete_transaction(message){
 	console.log('\n Starting complete transaction logic:')
-    // message.buyer_amount >= 2 * contract[message.uuid].seller_amount
     
     if (contract[message.uuid].buyer_amount >= 2 * contract[message.uuid].seller_amount){
         console.log('\n Verifying the contract_agreement...we gud.')
@@ -93,18 +89,18 @@ function complete_transaction(message){
     	console.log('\n Pay the seller the value 2 times the seller amount (1x for the value of the item and 1x for the collateral)')
     	var seller_pay = 2 * contract[message.uuid].seller_amount
 
-    	console.log('\n pay the buyer the rest of his collateral')
+    	console.log('\n pay the buyer the rest of his/her collateral')
     	var buyer_pay = contract[message.uuid].buyer_amount - contract[message.uuid].seller_amount
         
-        console.log('\n The contract is: ', contract[message.uuid])
-    	console.log('\n seller gets paid ', seller_pay)
-    	console.log('\n buyer gets paid ', buyer_pay)
+        console.log('\n The contract = ', contract[message.uuid])
+    	console.log('\n Seller gets paid: ', seller_pay)
+    	console.log('\n Buyer gets paid: ', buyer_pay)
     } else {
 
         console.log("\n The contact hasn't been agreed upon.")
         console.log(contract[message.uuid])
-        console.log('Return funds.')
-        return 'your funds'
+        console.log('Return buyer money.')
+        return 'buyer money'
 
     }
 
@@ -129,8 +125,8 @@ function seller(message){
 	else {
 		console.log('\n Seller information is already listed in the contract')
         console.log(contract[message.uuid])
-        console.log('Returning your money.')
-		return 'your money'
+        console.log('Returning seller money.')
+		return 'seller money'
 	}
 }
 
