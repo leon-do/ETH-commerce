087b10da98ff95860e8a87988b481308afc32c85
leondo <leondo123@gmail.com>
2017-09-28 22:08:15 -0600

    cancel sell is working

---
+++ misc/more misc/sendETH.js
@@ -0,0 +1,13 @@
+
+    function buy (){
+
+        web3.eth.sendTransaction({
+            value: $('#buyValue').val() * 1000000000000000000,
+            to: $("#buyContractAddress").val()
+        }, function(err, transactionHash) {
+          if (!err)
+            $('#buyTransactionHash').text(transactionHash)
+            console.log(transactionHash); // "0x0f92722137f0867bf55d643f7a2585597bb26bf22e72a457bf562d476ebe6b9b"
+        });
+
+    }
